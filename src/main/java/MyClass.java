d76b54e8b5ac5bffe2a53e736406ef1020d806c3
홍동우 <hdw9413@naver.com>
2022-05-22 14:23:09 +0900

    2nd commit

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,18 @@
+import java.util.Calendar;
+
+public class MyClass {
+
+
+    String name;
+
+    public MyClass(){
+        Calendar.getInstance();
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world!");
+
+    }
+}
